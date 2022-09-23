b30829786ec812abca51ca30c5bf436054bbc401
yinotherhino <muhammedbayero@gmail.com>
2022-09-23 19:52:31 +0100

    added modal and populated the frontend with random data

--- script/index.js
+++ script/index.js
@@ -1,11 +1,25 @@
 function main() {}
-
-$('#effects').click(function(){
+//top hamburger menu effect show
+$('#show-menu').click(function(){
     $('#show-effect').show(500);
 })
 
-$('#show-effect').click(function(){
+//top menu hide effect
+$('#hide-menu').click(function(){
     $('#show-effect').hide(500);
 });
 
+$('#yinohero').click(function(){
+    $('.modal').show();
+});
+
+$('#hide-modal').click(function(){
+    $('.modal').hide();
+})
+
+$('body').click(function(evt) {
+    if (evt.target.className == 'modal') {
+      $('.modal').hide();
+    }
+  });
 module.exports = { main }
