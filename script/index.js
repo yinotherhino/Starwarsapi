7c76b2677fb75e343d4294702cf420f4257f2c38
yinotherhino <muhammedbayero@gmail.com>
2022-09-23 13:48:15 +0100

    added frontend with some js effects

--- script/index.js
+++ script/index.js
@@ -1,3 +1,11 @@
 function main() {}
 
+$('#effects').click(function(){
+    $('#show-effect').show(500);
+})
+
+$('#show-effect').click(function(){
+    $('#show-effect').hide(500);
+});
+
 module.exports = { main }
