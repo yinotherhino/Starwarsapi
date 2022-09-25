fcc497bf1b1f018f6eef2bda7fb47fb6ab550063
yinotherhino <muhammedbayero@gmail.com>
2022-09-25 15:34:24 +0100

    css styling left

--- script/index.js
+++ script/index.js
@@ -1,23 +1,34 @@
-const main= () =>{
-    let result;
+const main= (() =>{
+    let APIresult;
     const images= ['lukesky.jpeg', 'c3po.jpeg', 'r2d2.webp', 'darth.jpeg', 'leia.jpeg', 'lars.png',
-                'beru.webp', 'r5.jpeg', 'biggs.jpeg', 'obi.jpeg'];
-    const resolveAPI= results=>{
-        if (results){
-        const contentLength= results.length;
-        results.forEach( (x,i) =>{
-        result= {name:x['name'], gender:x['gender'], height:x['height']};
-        document.getElementById("result-container").innerHTML+= '<div><img src="./img/'+images[i]+'" class="profile-pic"><p class="personname" data-name="'+ result.name+'" data-gender="'+result.gender+' " data-height="'+result.height+'">'+ result.name +'</p></div>';
-         });
-        modalClick(results);
-    }
-    }
+    'beru.webp', 'r5.jpeg', 'biggs.jpeg', 'obi.jpeg'];
+
     fetch('https://swapi.dev/api/people/')
     .then((response) => response.json())
     .then((data => {
         //send the results (array of individual characters) fetched from the API to the resolveAPI
-        resolveAPI(data['results']);
+        let results= data['results']
+        APIconsume(results);
     }));
+
+    const APIconsume= results=>{
+        if (results){
+        results.forEach( (x,i) =>{
+        APIresult= {name:x['name'], gender:x['gender'], height:x['height']};
+        document.getElementById("result-container").innerHTML+= 
+        `<div><img src="./img/${images[i]}" class="profile-pic">
+        <p class="personname" 
+            data-name="'${APIresult.name}'" 
+            data-gender="${APIresult.gender} " 
+            data-height="${APIresult.height}">
+        ${APIresult.name}
+        </p>
+        </div>`;
+         });
+        modalEvt();
+    }
+    }
+  
 
     
     //show the menu
@@ -25,18 +36,21 @@ const main= () =>{
         $('#show-effect').show(200);
     })
 
+    // document.getElementById("show-menu").onClick(function(){
+    //     document.getElementById("show-effect").style.display= "inline-block"
+    // })
+
     //top menu hide effect
     $('#hide-menu').click(function(){
         $('#show-effect').hide(100);
     });
 
-    const modalClick= (results)=>{
+    const modalEvt= ()=>{
     $('.personname').click(function(evt){
-        $('.modal-name').text(evt.target.dataset.name);
-        $('.modal-gender').text(evt.target.dataset.gender);
-        $('.modal-height').text(evt.target.dataset.height);
+        $('.modal-name').text("Name: "+evt.target.dataset.name);
+        $('.modal-gender').text('Gender: '+evt.target.dataset.gender);
+        $('.modal-height').text('Height: '+evt.target.dataset.height);
         $('.modal').show(100);
-
     });
     // $('.personname').click(function(evt){
     //     // console.log(results)
@@ -54,11 +68,9 @@ const main= () =>{
     });
 }
 
-}
+})()
 
 
-
-main();
 
 
 
